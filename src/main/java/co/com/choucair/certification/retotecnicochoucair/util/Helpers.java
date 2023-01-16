07be51b50bf75d1c43b3e647ec0dc178127d2b11
cristian <clopezm@choucairtesting.com>
2023-01-16 07:36:09 -0500

    Automatizacion Exito web y api

--- src/main/java/co/com/choucair/certification/retotecnicochoucair/util/Helpers.java
+++ src/main/java/co/com/choucair/certification/retotecnicochoucair/util/Helpers.java
@@ -10,8 +10,6 @@ import static co.com.choucair.certification.retotecnicochoucair.ui.ExitoUserInte
 import static co.com.choucair.certification.retotecnicochoucair.ui.ExitoUserInterface.TARGET_PRUEBA;
 
 public class Helpers {
-
-    public static String xpath = "(//span[@class='vtex-store-components-3-x-productBrand '])[%s]";
     private static Random random;
 
     static  {
@@ -37,59 +35,13 @@ public class Helpers {
         return getProducto(actor, limite, PRECIO_INDIVIDUAL);
     }
 
-    public static String getValorTodosProductos(){
-       return null;
-    }
 
 
-
-
-
-
-
-//    public String getNombreProducto(Actor actor, int limite){
-//        String result = "";
-//
-//        while(result.length() == 0) {
-//            int random =  Helpers.generarNumeroRandom(limite);
-//            result = actor.asksFor(Text.of(TARGET_PRUEBA.of(Integer.toString(random))));
-//
-//        }
-//        return result;
-//    }
-//
-//    public String getValorProducto(Actor actor, int limite){
-//        String valor = "";
-//        while(valor.length() == 0) {
-//            int random =  Helpers.generarNumeroRandom(limite);
-//
-//            valor = actor.asksFor(Text.of(PRECIO_INDIVIDUAL.of(Integer.toString(random))));
-//        }
-//        return valor;
-//    }
     public static int generarNumeroRandom(int limite) {
         return (random.nextInt(limite) + 1);
     }
 
 
-  /*  private Random random;
-    private HashSet<Integer> generatedNumbers;
-    private int limit;
-
-    public RandomNumberGenerator(int limit) {
-        this.random = new Random();
-        this.generatedNumbers = new HashSet<>();
-        this.limit = limit;
-    }
-
-    public int getUniqueRandom() {
-        int randomNumber;
-        do {
-            randomNumber = random.nextInt(limit);
-        } while (generatedNumbers.contains(randomNumber));
-        generatedNumbers.add(randomNumber);
-        return randomNumber;
-    }*/
 
 
 
